c1aa0e149ed6bc81b64b016689b4da71c5b3c078
honanaipr <honanaipr@github.com>
2023-10-18 17:29:48 +0300

    add panel(ist)

--- src/components/List.vue
+++ src/components/List.vue
@@ -8,38 +8,56 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
-  <ListItem>
-    <template #heading>Игла спинальная 26G (90 мм) Portex Пенсил с иглой-интродьюсером 20G</template>
-    <template #amount>12</template>
-    <template #min_amount>10</template>
+  <article class="panel is-primary">
+    <!-- <p class="panel-tabs">
+        <a class="is-active">All</a>
+        <a>Public</a>
+        <a>Private</a>
+        <a>Sources</a>
+        <a>Forks</a>
+      </p> -->
+    <div class="panel-block">
+      <p class="control has-icons-left">
+        <input class="input is-primary" type="text" placeholder="Search">
+        <span class="icon is-left">
+          <i class="fas fa-search" aria-hidden="true"></i>
+        </span>
+      </p>
+    </div>
 
-  </ListItem>
+    <ListItem>
+      <template #heading>Игла спинальная 26G (90 мм) Portex Пенсил с иглой-интродьюсером 20G</template>
+      <template #amount>12</template>
+      <template #min_amount>10</template>
 
-  <ListItem>
-    <template #heading>Набор для спинально-эпидуральной 
-анестезии Portex</template>
-    <template #amount>12</template>
-    <template #min_amount>10</template>
-  </ListItem>
+    </ListItem>
 
-  <ListItem>
-    <template #heading>Игла спинальная (88мм) SURU для 
-анестезии Квинке</template>
-    <template #amount>12</template>
-    <template #min_amount>10</template>
-  </ListItem>
+    <ListItem>
+      <template #heading>Набор для спинально-эпидуральной
+        анестезии Portex</template>
+      <template #amount>12</template>
+      <template #min_amount>10</template>
+    </ListItem>
 
-  <ListItem>
-    <template #heading>Медиагель - гель для УЗИ высокой 
-вязкости бесцветный</template>
-    <template #amount>12</template>
-    <template #min_amount>10</template>
-  </ListItem>
+    <ListItem>
+      <template #heading>Игла спинальная (88мм) SURU для
+        анестезии Квинке</template>
+      <template #amount>12</template>
+      <template #min_amount>10</template>
+    </ListItem>
 
-  <ListItem>
-    <template #heading>Набор для спинально-эпидуральной 
-анестезии Portex</template>
-    <template #amount>12</template>
-    <template #min_amount>10</template>
-  </ListItem>
+    <ListItem>
+      <template #heading>Медиагель - гель для УЗИ высокой
+        вязкости бесцветный</template>
+      <template #amount>12</template>
+      <template #min_amount>10</template>
+    </ListItem>
+
+    <ListItem>
+      <template #heading>Набор для спинально-эпидуральной
+        анестезии Portex</template>
+      <template #amount>12</template>
+      <template #min_amount>10</template>
+    </ListItem>
+  </article>
 </template>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import List from '../components/List.vue'
+import ListItem from '../components/ListItem.vue';
 </script>
 
 <template>
