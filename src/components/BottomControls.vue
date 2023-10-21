96e4131b3cbd02859ee3c76cbfded553d6affe0c
honanaipr <honanaipr@github.com>
2023-10-21 10:47:27 +0300

    patch bottom controls

--- src/components/BottomControls.vue
+++ src/components/BottomControls.vue
@@ -1,23 +1,52 @@
 <script setup>
-
+import HomeIcon from "../components/icons/HomeIcon.vue"
+import TruckIcon from "../components/icons/TruckIcon.vue"
+import CrossIcon from "../components/icons/CrossIcon.vue"
+import TrashIcon from "../components/icons/TrashIcon.vue"
+import ProfileIcon from "../components/icons/ProfileIcon.vue" 
 </script>
       
 <template>
     <nav class="level is-mobile bottom-controls">
         <div class="level-item has-text-centered">
-            <RouterLink to="/">Главная</RouterLink>
+            <RouterLink to="/"><span class="icon-text">
+                    <span class="icon">
+                        <HomeIcon />
+                    </span>
+                    <span>Главная</span>
+                </span></RouterLink>
         </div>
         <div class="level-item has-text-centered">
-            <RouterLink to="/about">В пути</RouterLink>
+            <RouterLink to="/transit"><span class="icon-text">
+                    <span class="icon">
+                        <TruckIcon />
+                    </span>
+                    <span>В пути</span>
+                </span></RouterLink>
         </div>
         <div class="level-item has-text-centered">
-            <RouterLink to="/about">Добавить</RouterLink>
+            <RouterLink to="/add"><span class="icon-text">
+                    <span class="icon">
+                        <CrossIcon />
+                    </span>
+                    <span>Добавить</span>
+                </span></RouterLink>
         </div>
         <div class="level-item has-text-centered">
-            <RouterLink to="/basket">Корзина</RouterLink>
+            <RouterLink to="/basket"><span class="icon-text">
+                    <span class="icon">
+                        <TrashIcon />
+                    </span>
+                    <span>Корзина</span>
+                </span></RouterLink>
         </div>
         <div class="level-item has-text-centered">
-            <RouterLink to="/profile">Профиль</RouterLink>
+            <RouterLink to="/profile"><span class="icon-text">
+                    <span class="icon">
+                        <ProfileIcon />
+                    </span>
+                    <span>Профиль</span>
+                </span></RouterLink>
         </div>
     </nav>
 </template>
@@ -25,9 +54,12 @@
 <style scoped>
 .bottom-controls {
     z-index: 1;
-    background-color: aliceblue;
+    background-color: white;
     position: fixed;
     bottom: 0;
     width: 100%;
+    margin-bottom: 0;
+    padding-top: 1rem;
+    padding-bottom: 1.5rem;
 }
 </style>
