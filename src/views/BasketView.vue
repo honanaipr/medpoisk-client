9a671c27e58f8e622cbbc2c2c0c56b68042fbc2a
honanaipr <honanaipr@github.com>
2023-10-21 08:53:59 +0300

    add basket view

---
+++ src/views/BasketView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import List from '../components/List.vue'
+import ListItem from '../components/ListItem.vue';
+</script>
+
+<template>
+  <main>
+    <List/>
+  </main>
+</template>
