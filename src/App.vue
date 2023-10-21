423efd43a1050ae9c0da8f0adf5161a6aa519560
honanaipr <honanaipr@github.com>
2023-10-21 10:29:39 +0300

    Center banner

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import BottomControls from './components/BottomControls.vue';
+import Banner from './components/Banner.vue';
 </script>
 
 <template>
   <div class="container">
-    <nav class="navbar is-fixed-top">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img src="@/assets/logo.png" width="112" height="28">
-        </a>
-      </div>
-    </nav>
+    <Banner/>
     <BottomControls/>
     <RouterView />
   </div>
 </template>
 
 <style scoped>
+body {
+  font-family: 'Roboto';
+}
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navbar is-fixed-top">
+        <nav class="level is-mobile">
+            <div class="level-item has-text-centered">
+                <img src="@/assets/logo.png" width="112" height="28">
+            </div>
+        </nav>
+    </nav>
+</template>
+
+<style scoped>
+</style>
