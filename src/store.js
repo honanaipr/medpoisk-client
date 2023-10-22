683e8179891a9507edaa8bf872e37afa066289e3
honanaipr <honanaipr@github.com>
2023-10-22 20:40:43 +0300

    repare facking stupid error

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { ref } from 'vue'
 import { v4 as uuid4 } from 'uuid'
 import { faker } from '@faker-js/faker'
+
 function createItem(){
   let id = uuid4()
   let heading = faker.commerce.productDescription()
@@ -15,8 +16,8 @@ export const store = ref({
 })
 
 export function toBasket(id) {
-  store.value.list.splice(store.value.list.findIndex((item) => !(item.id == id)), 1)
   store.value.basket.push(store.value.list.find((item) => (item.id == id)))
+  store.value.list.splice(store.value.list.findIndex((item) => !(item.id == id))-1, 1)
 }
 
 export function addItem(heading, amount, min_amount) {
