ad8425dac8c662b313bfb199690b3e303f80b65b
honanaipr <honanaipr@github.com>
2023-10-22 20:03:32 +0300

    integrate faker generation

--- src/store.js
+++ src/store.js
@@ -1,77 +1,25 @@
 import { ref } from 'vue'
-import { v4 as uuid4 } from 'uuid';
+import { v4 as uuid4 } from 'uuid'
+import { faker } from '@faker-js/faker'
+function createItem(){
+  let id = uuid4()
+  let heading = faker.commerce.productDescription()
+  let amount = faker.number.int({max:40, min:1})
+  let min_amount = faker.number.int({max:amount, min:1})
+  return {id: id, heading: heading, amount:amount, min_amount:min_amount}
+}
 export const store = ref({
-  list: [
-    {
-      id: uuid4(),
-      heading: "Игла спинальная 26G (90 мм) Portex Пенсил с иглой-интродьюсером 20G",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Набор для спинально-эпидуральной анестезии Portex",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Игла спинальная (88мм) SURU для анестезии Квинке",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Медиагель - гель для УЗИ высокой вязкости бесцветный",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Набор для спинально-эпидуральной анестезии Portex",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Игла спинальная 26G (90 мм) Portex Пенсил с иглой-интродьюсером 20G",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Набор для спинально-эпидуральной анестезии Portex",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Игла спинальная (88мм) SURU для анестезии Квинке",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Медиагель - гель для УЗИ высокой вязкости бесцветный",
-      amount: 12,
-      min_amount: 10,
-    },
-    {
-      id: uuid4(),
-      heading: "Набор для спинально-эпидуральной анестезии Portex",
-      amount: 12,
-      min_amount: 10,
-    },
-  ],
+  list: Array.from({ length: 5 }, createItem),
+
   basket: [],
 })
 
-export function toBasket(id){
-  store.value.list.splice(store.value.list.findIndex((item) => !(item.id == id)),1)
+export function toBasket(id) {
+  store.value.list.splice(store.value.list.findIndex((item) => !(item.id == id)), 1)
   store.value.basket.push(store.value.list.find((item) => (item.id == id)))
 }
 
-export function addItem(heading, amount, min_amount){
+export function addItem(heading, amount, min_amount) {
   store.list.push({
     id: uuid4(),
     heading: heading,
