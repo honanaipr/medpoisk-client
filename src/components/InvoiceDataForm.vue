8908d2fbe990ef2f78c0504f5a4408bfa5d9e979
honanaipr <honanaipr@github.com>
2023-10-23 15:00:06 +0300

    Add InvoiceView

---
+++ src/components/InvoiceDataForm.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const invoiceNumber = ref(null)
+const invoiceData = ref(null)
+const processedItems = ref(0)
+const totalItems = ref(0)
+
+const heading = computed(() => {
+    return "Накладная №" + (invoiceNumber.value || "?") + " от " + (invoiceData.value || "?")
+})
+</script>
+
+<template>
+    <div class="container is-fluid">
+        <div class="content">
+            <h1>{{ heading }}</h1>
+        </div>
+        <div class="field">
+            <label class="label">Накладная №:</label>
+            <div class="control">
+                <input class="input" type="text" v-model="invoiceNumber">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">Накладная №:</label>
+            <div class="control">
+                <input class="input" type="date" v-model="invoiceData">
+            </div>
+        </div>
+
+        <button class="button is-fullwidth" disabled> Обработанно позиций: {{ processedItems }}/{{ totalItems }}</button>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/InvoiceView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import List from '../components/List.vue'
+import { store } from '../store.js'
+import PlusIcon from '../components/icons/PlusIcon.vue';
+import InvoiceDataForm from '../components/InvoiceDataForm.vue';
+import { ref } from 'vue';
+
+let incoiceList = ref([])
+
+</script>
+
+<template>
+    <InvoiceDataForm />
+    <List :source="incoiceList" />
+
+    <button class="button is-large is-fullwidth">
+        <PlusIcon />
+    </button>
+</template>
