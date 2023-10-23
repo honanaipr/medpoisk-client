d1bee4654b13e02ed6e3f847c54491f843f62753
honanaipr <honanaipr@github.com>
2023-10-23 14:56:36 +0300

    add CodeScanerView

---
+++ src/views/CodeScanerView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import List from '../components/List.vue'
+import { store } from '../store.js'
+import PlusIcon from '../components/icons/PlusIcon.vue';
+import InvoiceDataForm from '../components/InvoiceDataForm.vue';
+import { ref } from 'vue';
+
+let incoiceList = ref([])
+
+</script>
+
+<template>
+    <div class="container is-align-content-center is-fluid">
+        <div class="content">
+            <h1>Отсканируйте код</h1>
+        </div>
+        <figure class="image">
+            <img src="@/assets/qrscan.png">
+        </figure>
+    </div>
+</template>
+
+<style scoped>
+figure.image {
+    margin-right: 5rem;
+    margin-left: 5rem;
+    margin-top: 7rem;
+}
+</style>
