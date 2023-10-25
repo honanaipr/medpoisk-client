04cd1a3f62955c758ca9788da04a3cbbc0c8469b
honanaipr <honanaipr@github.com>
2023-10-25 11:08:58 +0300

    add recipient information to basket

---
+++ src/components/WriteOffFor.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { store } from '../store.js'
+
+// defineProps(['room_id', 'doctor_id'])
+defineProps({
+    room_id: {
+        default: ""
+    },
+    doctor_id: {
+        default: ""
+    },
+    allow_apply: {
+
+    }
+})
+
+defineEmits(['update:room_id', 'update:doctor_id', 'apply', 'cancel'])
+
+</script>
+
+<template>
+    <div class="container is-fluid">
+        <div class="box item">
+            <div class="field">
+                <label class="label">Врач:</label>
+                <div class="control">
+                    <div class="select is-fullwidth" :class="{ 'is-danger': !doctor_id }">
+                        <select :value="doctor_id" @input="$emit('update:doctor_id', $event.target.value)">
+                            <option disabled value="">Выбрать врача</option>
+                            <option v-for="doctor in store.doctors" :key="doctor.id" :value="doctor.id">{{ doctor.name }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+                <p class="help is-danger" v-if="!doctor_id">Поле необходимо</p>
+            </div>
+            <div class="field">
+                <label class="label">Кабинет:</label>
+                <div class="control">
+                    <div class="select is-fullwidth" :class="{ 'is-danger': !room_id }">
+                        <select :value="room_id" @input="$emit('update:room_id', $event.target.value)">
+                            <option disabled value="">Выбрать врача</option>
+                            <option v-for="room in store.rooms" :key="room.id" :value="room.id">{{ room.number }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+                <p class="help is-danger" v-if="!room_id">Поле необходимо</p>
+            </div>
+        </div>
+        <div class="buttons is-fullwidth">
+            <button class="button is-fullwidth is-danger is-light" style="border-color: black;"
+                @click="$emit('cancel')">Отменить</button>
+            <button class="button is-fullwidth is-primary is-light" style="border-color: black;" @click="$emit('apply')"
+                :disabled="!(!!room_id && !!doctor_id && allow_apply)">Списать</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.item {
+    margin-bottom: 0.5rem;
+    border: 1px solid black;
+    padding: 0.5rem;
+    border-radius: 5px;
+}
+
+.item-control {
+    width: 5rem;
+}
+
+p.place-tag {
+    margin-bottom: 0;
+}
+</style>
--- src/views/BasketView.vue
+++ src/views/BasketView.vue
@@ -1,8 +1,31 @@
 <script setup>
 import List from '../components/List.vue'
-import { store } from '../store.js'
+import WriteOffFor from '../components/WriteOffFor.vue';
+import { store, backToList, writeOff } from '../store.js'
+import { ref } from 'vue';
+// import router from '../router'
+
+const doctor_id = ref("")
+const room_id = ref("")
+
+function apply() {
+  writeOff(doctor_id, room_id)
+  doctor_id.value = null
+  room_id.value = null
+}
+
+function cancel() {
+  while (store.value.basket.length){
+    backToList(store.value.basket[0].id)
+  }
+  doctor_id.value = null
+  room_id.value = null
+}
+
 </script>
 
 <template>
   <List :source="store.basket" />
+  <WriteOffFor v-model:doctor_id="doctor_id" v-model:room_id="room_id" :allow_apply="!!store.basket.length" @apply="apply"
+    @cancel="cancel" />
 </template>
