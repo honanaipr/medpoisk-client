e52bfcccc6dc69a826e6ca3934fb0f962e05a0a5
honanaipr <honanaipr@github.com>
2023-10-25 04:44:11 +0300

    ugly stub for profile and transit views

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,12 +1,21 @@
 <script setup>
-import List from '../components/List.vue'
-import ListItem from '../components/ListItem.vue';
+import WrenchIcon from '../components/icons/WrenchIcon.vue';
 </script>
 
 <template>
-  <div class="container is-fluid">
-    <div class="content">
-      <h3>ProfileView</h3>
+  <div class="container is-fluid is-multiline" style="padding-top: 10rem;">
+    <div class="columns is-mobile is-centered">
+
+      <div class="column is-half">
+        <WrenchIcon style="display: block; height: 10rem; margin: auto;" />
+      </div>
+
+    </div>
+    <div class="columns is-mobile is-centered" style="margin-top: 5rem;">
+      <div class="content">
+        <h3>&lt;501&gt;  Not Implemented</h3>
+        <h3>ProfileView</h3>
+      </div>
     </div>
   </div>
 </template>
