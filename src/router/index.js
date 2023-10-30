56f3e95661b69345f67914cd867b6013bd1f3d4f
honanaipr <honanaipr@github.com>
2023-10-30 18:46:05 +0300

    remove unused imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ListView from '../views/ListView.vue'
-import { store } from '../store.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/CodeScanerView.vue
+++ src/views/CodeScanerView.vue
@@ -1,12 +1,5 @@
 <script setup>
-import List from '../components/List.vue'
-import { store } from '../store.js'
-import PlusIcon from '../components/icons/PlusIcon.vue';
-import InvoiceDataForm from '../components/InvoiceDataForm.vue';
 import { ref } from 'vue';
-
-let incoiceList = ref([])
-
 </script>
 
 <template>
