51a0e3b60298b160ecb4c416f5879e1f111d6335
honanaipr <honanaipr@github.com>
2024-01-10 00:07:19 +0300

    add pathes

---
+++ src/pathes.js
@@ -0,0 +1,11 @@
+export const API_PATH = 'http://localhost:8000/api/v0/'
+
+export const API_PLACE_PATH = `${API_PATH}place`
+export const API_DOCTOR_PATH = `${API_PATH}doctor`
+export const API_ROOM_PATH = `${API_PATH}room`
+export const API_POSITION_PATH = `${API_PATH}position`
+export const API_PRODUCT_PATH = `${API_PATH}product`
+
+export const API_AUTH_PATH = `${API_PATH}auth/`
+export const API_AUTH_LOGIN_PATH = `${API_AUTH_PATH}login`
+export const API_AUTH_REFRESH_PATH = `${API_AUTH_PATH}refresh`
