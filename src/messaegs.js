8401a4181fe85e73ba0aba2d781eb817b79d0215
honanaipr <honanaipr@github.com>
2024-01-10 00:07:42 +0300

    add messages list

---
+++ src/messaegs.js
@@ -0,0 +1,21 @@
+export default {
+    PLACE_ADD_ERROR_MESSAGE : "Ошибка добавления места хранения!",
+    PLACE_ADD_OK_MESSAGE : "Место хранения добавлено!",
+    PLACE_UPDATE_ERROR_MESSAGE : "Ошибка обновления мест хранения!",
+    PLACE_UPDATE_OK_MESSAGE : "Список мест хранения обновлен!",
+    
+    DOCTOR_ADD_ERROR_MESSAGE : "Ошибка добавления врача!",
+    DOCTOR_ADD_OK_MESSAGE : "Врач добавлен!",
+    DOCTOR_UPDATE_ERROR_MESSAGE : "Ошибка обновления списка врачей!",
+    DOCTOR_UPDATE_OK_MESSAGE : "Список врачей обновлен!",
+    
+    ROOM_ADD_ERROR_MESSAGE : "Ошибка добавления кабинета!",
+    ROOM_ADD_OK_MESSAGE : "Кабинет добавлен!",
+    ROOM_UPDATE_ERROR_MESSAGE : "Ошибка обновления списка кабинетов!",
+    ROOM_UPDATE_OK_MESSAGE : "Список кабинетов обновлен!",
+    
+    PRODUCT_ADD_ERROR_MESSAGE : "Ошибка добавления продукта!",
+    PRODUCT_ADD_OK_MESSAGE : "Продукт добавлен!",
+    PRODUCT_UPDATE_ERROR_MESSAGE : "Ошибка обновления списка продуктов!",
+    PRODUCT_UPDATE_OK_MESSAGE : "Список продуктов обновлен!",
+}
