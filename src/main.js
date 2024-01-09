768488cd7a919f1b5a55c4b1f45a239bb41aea1d
honanaipr <honanaipr@github.com>
2024-01-09 20:28:35 +0300

    add pinia to project

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import './assets/bulma_config.sass'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { IonicVue } from '@ionic/vue';
+import { createPinia } from 'pinia'
+import { IonicVue } from '@ionic/vue'
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(IonicVue)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
