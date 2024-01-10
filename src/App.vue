6ab132f522a861e8b33dd5fd03e6580777bafa8c
honanaipr <honanaipr@github.com>
2024-01-10 05:57:31 +0300

    init new stores on start

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,39 @@ import { RouterView } from 'vue-router'
 import BottomControls from './components/BottomControls.vue';
 import BannerComponent from './components/BannerComponent.vue';
 import AddTypeModal from "./components/AddTypeModal.vue"
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { usePlaceStore } from './stores/place_store';
+import { useRoomStore } from './stores/room_store';
+import { useDoctorStore } from './stores/doctor_store';
+import { useProductStore } from './stores/product_store';
+import { useAuthStore } from './stores/auth_store';
+import { useInventoryStore } from './stores/inventory_store';
+
+
+const place_store = usePlaceStore()
+const room_store = useRoomStore()
+const doctor_store = useDoctorStore()
+const product_store = useProductStore()
+const auth_store = useAuthStore()
+const inventory_store = useInventoryStore()
+
+onMounted(async () => {
+  try {
+    await auth_store.update()
+    console.log("authorized")
+  } catch {
+    console.log("not authorized")
+  }
+  Promise.all([
+    place_store.update(),
+    room_store.update(),
+    doctor_store.update(),
+    product_store.update(),
+    inventory_store.update(),
+  ])
+    .then(() => console.log("sucsess"))
+    .catch((error) => console.log(error))
+})
 
 let isModalActive = ref(false)
 </script>
