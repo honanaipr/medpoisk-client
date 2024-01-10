61c548117b267b63fcac8177d4c80c055893724d
honanaipr <honanaipr@github.com>
2024-01-10 06:01:42 +0300

    rename list view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ListView from '../views/ListView.vue'
+import InventoryView from '../views/InventoryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: ListView,
+      component: InventoryView,
       meta: { transition: 'slide-left' },
     },
     {
