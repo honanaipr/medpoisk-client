7a229a33612305980e0c868483d21a35b27a08c6
honanaipr <honanaipr@github.com>
2024-01-10 03:46:43 +0300

    change doctors endpoint

--- src/pathes.js
+++ src/pathes.js
@@ -1,7 +1,7 @@
 export const API_PATH = 'http://localhost:8000/api/v0/'
 
 export const API_PLACE_PATH = `${API_PATH}place`
-export const API_DOCTOR_PATH = `${API_PATH}doctor`
+export const API_DOCTOR_PATH = `${API_PATH}profile/staff`
 export const API_ROOM_PATH = `${API_PATH}room`
 export const API_POSITION_PATH = `${API_PATH}position`
 export const API_PRODUCT_PATH = `${API_PATH}product`
