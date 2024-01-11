bb46a936663a916221deca2a0864d5d167845b74
honanaipr <honanaipr@github.com>
2024-01-12 01:16:50 +0300

    patch schemas

--- src/schemas.js
+++ src/schemas.js
@@ -2,7 +2,11 @@ import Joi from 'joi'
 
 export const doctorSchema = Joi.object({
   id: Joi.number(),
-  name: Joi.string()
+  username: Joi.string(),
+  email: Joi.string().allow(null),
+  first_name: Joi.string().allow(null),
+  middle_name: Joi.string().allow(null),
+  last_name: Joi.string()
 })
 
 export const roomSchema = Joi.object({
@@ -10,7 +14,7 @@ export const roomSchema = Joi.object({
   title: Joi.string(),
   division: Joi.object({
     id: Joi.number(),
-    title: Joi.string(),
+    title: Joi.string()
   })
 })
 
@@ -18,6 +22,11 @@ export const placeSchema = Joi.object({
   id: Joi.number(),
   title: Joi.string(),
   division_id: Joi.number()
+})
+
+export const placeShortSchema = Joi.object({
+  id: Joi.number(),
+  title: Joi.string()
 })
 
 export const productSchema = Joi.object({
@@ -28,8 +37,30 @@ export const productSchema = Joi.object({
   pictures: Joi.array().items(Joi.object({ url: Joi.string() }))
 })
 
+export const productShortSchema = Joi.object({
+  id: Joi.number(),
+  title: Joi.string()
+})
+
 export const positionSchema = Joi.object({
   product_id: Joi.number(),
   place_id: Joi.number(),
   amount: Joi.number()
+})
+
+export const inventoryItemSchema = Joi.object({
+  product: productShortSchema,
+  place: placeShortSchema,
+  amount: Joi.number()
+})
+
+export const divisionSchema = Joi.object({
+  id: Joi.number(),
+  title: Joi.string(),
+})
+
+export const limitSchema = Joi.object({
+  division_id: Joi.number(),
+  product_id: Joi.number(),
+  min_amount: Joi.number(),
 })
