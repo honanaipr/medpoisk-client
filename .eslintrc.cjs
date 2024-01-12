2d2ef27856a4b5e5714b4851ff2d966661453a4b
honanaipr <honanaipr@github.com>
2024-01-12 05:33:58 +0300

    copy configuration from fresh product

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,10 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:@typescript-eslint/recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
-  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: "@typescript-eslint/parser" 
+    ecmaVersion: 'latest'
   }
 }
