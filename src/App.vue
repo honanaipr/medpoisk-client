ae1904fdc324ce3750813104b76d73afa1e84ddc
honanaipr <honanaipr@github.com>
2024-01-17 14:27:31 +0300

    add auth

--- src/App.vue
+++ src/App.vue
@@ -3,47 +3,18 @@ import { RouterView } from 'vue-router'
 import BottomControls from './components/BottomControls.vue'
 import BannerComponent from './components/BannerComponent.vue'
 import AddTypeModal from './components/AddTypeModal.vue'
-import { ref, onMounted } from 'vue'
-import { usePlaceStore } from './stores/place_store'
-import { useRoomStore } from './stores/room_store'
-import { useDoctorStore } from './stores/doctor_store'
-import { useProductStore } from './stores/product_store'
+import AuthViewVue from './views/AuthView.vue'
+import { ref } from 'vue'
 import { useAuthStore } from './stores/auth_store'
-import { useInventoryStore } from './stores/inventory_store'
-import { useLimitStore } from './stores/limit_store'
 
-const place_store = usePlaceStore()
-const room_store = useRoomStore()
-const doctor_store = useDoctorStore()
-const product_store = useProductStore()
 const auth_store = useAuthStore()
-const inventory_store = useInventoryStore()
-const limit_store = useLimitStore()
-
-onMounted(async () => {
-  try {
-    await auth_store.update()
-    console.log('authorized')
-  } catch {
-    console.log('not authorized')
-  }
-  Promise.all([
-    place_store.update(),
-    room_store.update(),
-    doctor_store.update(),
-    product_store.update(),
-    inventory_store.update(),
-    limit_store.update(),
-  ])
-    .then(() => console.log('sucsess'))
-    .catch((error) => console.log(error))
-})
 
 let isModalActive = ref(false)
 </script>
 
 <template>
   <div
+    v-if="auth_store.username"
     style="display: flex; height: 100dvh; width: 100vw; flex-direction: column; overflow: hidden"
     class="app"
   >
@@ -53,6 +24,7 @@ let isModalActive = ref(false)
     </div>
     <BottomControls @open-add-modal="isModalActive = true" />
   </div>
+  <AuthViewVue v-else/>
   <AddTypeModal :is-active="isModalActive" @close="isModalActive = false" />
 </template>
 
--- src/stores/auth_store.ts
+++ src/stores/auth_store.ts
@@ -1,18 +1,51 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import type { Ref } from 'vue'
 import * as jose from 'jose'
 import axios from 'axios'
-import { API_AUTH_LOGIN_PATH } from '../pathes'
+import { API_AUTH_LOGIN_PATH, API_AUTH_REFRESH_PATH } from '../pathes'
 import { tokenDataSchema } from '@/schemas'
+import { usePlaceStore } from '@/stores/place_store'
+import { useRoomStore } from '@/stores/room_store'
+import { useDoctorStore } from '@/stores/doctor_store'
+import { useProductStore } from '@/stores/product_store'
+import { useInventoryStore } from '@/stores/inventory_store'
+import { useLimitStore } from '@/stores/limit_store'
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref(null)
   const exp = ref(Date.now())
+  const username = ref('')
+  const roles: Ref<{division_id: number, role_name: string}[]> = ref([])
 
   async function update() {
+    return axios({
+      method: 'POST',
+      url: API_AUTH_REFRESH_PATH,
+      withCredentials: true,
+    })
+      .then((response) => {
+        token.value = response.data.access_token
+        const token_data = jose.decodeJwt(response.data.access_token)
+        const joiResult = tokenDataSchema.validate(token_data)
+        if (!joiResult.error) {
+          exp.value = joiResult.value.exp
+          username.value = joiResult.value.username
+          roles.value = joiResult.value.roles
+        } else {
+          throw Error('Token data incorrect ' + joiResult.error.message)
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        throw error
+      })
+  }
+
+  async function login(login: string, password: string) {
     const params = new URLSearchParams()
-    params.append('username', 'petrovna_rus')
-    params.append('password', 'petrovna_rus')
+    params.append('username', login)
+    params.append('password', password)
 
     return axios({
       method: 'POST',
@@ -26,15 +59,38 @@ export const useAuthStore = defineStore('auth', () => {
         const joiResult = tokenDataSchema.validate(token_data)
         if (!joiResult.error) {
           exp.value = joiResult.value.exp
+          username.value = joiResult.value.username
+          roles.value = joiResult.value.roles
         } else {
           throw Error('Token data incorrect ' + joiResult.error.message)
         }
+        const place_store = usePlaceStore()
+        const room_store = useRoomStore()
+        const doctor_store = useDoctorStore()
+        const product_store = useProductStore()
+        const inventory_store = useInventoryStore()
+        const limit_store = useLimitStore()
+        Promise.all([
+          place_store.update(),
+          room_store.update(),
+          doctor_store.update(),
+          product_store.update(),
+          inventory_store.update(),
+          limit_store.update(),
+        ])
+          .then(() => console.log('sucsess'))
+          .catch((error) => console.log(error))
       })
       .catch((error) => {
         console.log(error)
         throw error
       })
   }
+
+  async function logout() {
+    console.log("Not implimented")
+  }
+
   async function getFreshToken() {
     const now = new Date(Date.now()).getSeconds()
     if (!token.value || exp.value < now) {
@@ -43,5 +99,5 @@ export const useAuthStore = defineStore('auth', () => {
     return token.value
   }
 
-  return { token, getFreshToken, update }
+  return { token, getFreshToken, update, login, logout, username, roles }
 })
