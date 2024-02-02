286c6119a133aca8d81ceeae02134fb92e1be117
honanaipr <honanaipr@gmail.com>
2024-02-02 12:51:08 +0300

    profile view adjusted to design

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,57 +1,35 @@
 <script setup lang="ts">
-import { usePlaceStore } from '@/stores/place_store';
-import { useDoctorStore } from '@/stores/doctor_store';
-import { useRoomStore } from '@/stores/room_store';
-import { Doctor, Room, Place } from '@/types';
-import { ref } from 'vue'
-
-const newPlaceTitle = ref(null)
-const newDoctorName = ref(null)
-const newRoomNumber = ref(null)
-
-const place_store = usePlaceStore()
-const doctor_store = useDoctorStore()
-const room_store = useRoomStore()
-
+import ButtonComponent from '@/components/ButtonComponent.vue';
 </script>
 
 <template>
-  <div class="container is-fluid is-multiline" style="padding-top: 5rem;">
-    <div class="field">
-      <label class="label">Номер кабинета:</label>
-      <div class="control">
-        <input class="input" v-model="newRoomNumber" type="text">
-      </div>
-    </div>
-    <div class="field">
-      <div class="control">
-        <button class="button is-link" @click="room_store.addRoom(new Room({title: newRoomNumber})); newRoomNumber=null">Добавить кабинет</button>
-      </div>
-    </div>
-
-    <div class="field">
-      <label class="label">Имя врача:</label>
-      <div class="control">
-        <input class="input" v-model="newDoctorName" type="text">
-      </div>
-    </div>
-    <div class="field">
-      <div class="control">
-        <button class="button is-link" @click="doctor_store.addDoctor(new Doctor({name: newDoctorName})); newDoctorName=null">Добавить врача</button>
-      </div>
-    </div>
-
-
-    <div class="field">
-      <label class="label">Место хранения:</label>
-      <div class="control">
-        <input class="input" v-model="newPlaceTitle" type="text">
-      </div>
-    </div>
-    <div class="field">
-      <div class="control">
-        <button class="button is-link" @click="place_store.addPlace(new Place({title: newRoomNumber})); newPlaceTitle=null">Добавить место хранения</button>
-      </div>
-    </div>
+  <div class="header">
+    <h1>Название организации</h1>
+    <h2>Название подразделения</h2>
+  </div>
+  <div class="container">
+    <ButtonComponent title="Настройки профиля" @click="()=>{$router.push({name: 'home'})}" has-border />
+    <ButtonComponent title="Журнал накладных" @click="()=>{$router.push({name: 'home'})}" />
+    <ButtonComponent title="Журнал списания" @click="()=>{$router.push({name: 'home'})}" />
+    <ButtonComponent title="Места хранения" @click="()=>{$router.push({name: 'home'})}" />
+    <ButtonComponent title="Врачи" @click="()=>{$router.push({name: 'home'})}" />
+    <ButtonComponent title="Кабинеты" @click="()=>{$router.push({name: 'home'})}" />
   </div>
 </template>
+<style scoped lang="sass">
+.container
+  display: flex
+  flex-direction: column
+  gap: 16px
+  padding: 0 10px 0 10px
+.header
+  padding: 0 10px 0 10px
+  color: #121212
+  font-weight: bold
+.header h1
+  font-size: 20px
+  margin-bottom: 16px
+.header h2
+  font-size: 16px
+  margin-bottom: 16px
+</style>
