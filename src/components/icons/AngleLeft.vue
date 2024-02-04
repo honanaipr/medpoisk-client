9cb714d55bc8224562e4c1a218f0574bb27d225c
honanaipr <rubtm.r@gmail.com>
2024-02-05 00:57:26 +0300

    add icons

---
+++ src/components/icons/AngleLeft.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M10.546 14.6682L4.43935 8.56152L10.546 2.44152L8.66602 0.561523L0.666016 8.56152L8.66602 16.5615L10.546 14.6682Z"
+            fill="#121212" />
+    </svg>
+</template>
