ec0dc6936b8607f2d379049a38e34175f282b13c
honanaipr <rubtm.r@gmail.com>
2024-02-04 22:45:48 +0300

    add custom section component

---
+++ src/components/common/SectionComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped lang="sass">
+.section
+  background-color: var(--background-seconfary)
+  padding: 16px
+  border-radius: 8px
+  display: flex
+  flex-direction: column
+  gap: 16px
+  font-size: 14px
+  font-weight: 600
+  color: var(--text-black)
+</style>
