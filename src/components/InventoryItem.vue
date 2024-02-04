373941b30c38f5898a51d92c5467fd7d2e1c610e
honanaipr <honanaipr@gmail.com>
2024-02-04 13:53:51 +0300

    remove listComponent from inventory view

---
+++ src/components/InventoryItem.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { ListItem } from '@/types'
+import MoreIcon from './icons/MoreIcon.vue'
+import FavoriteIcon from './icons/FavoriteIcon.vue'
+
+const emit = defineEmits(['doubleClick'])
+
+const props = defineProps({
+  listItem: { type: ListItem, required: true },
+})
+
+const amount = computed(() => {
+  return props.listItem.amount
+})
+
+const places = computed(() => {
+  return props.listItem.places
+})
+
+const detectDoubleTapClosure = (() => {
+  let lastTap = 0
+  let timeout
+  return function detectDoubleTap(event) {
+    const curTime = new Date().getTime()
+    const tapLen = curTime - lastTap
+    if (tapLen < 300 && tapLen > 0) {
+      emit('doubleClick')
+      event.preventDefault()
+    } else {
+      timeout = setTimeout(() => {
+        clearTimeout(timeout)
+      }, 500)
+    }
+    lastTap = curTime
+  }
+})()
+</script>
+
+<template>
+  <div
+    class="inventory-item"
+    @dblclick="$router.push({ name: 'product', params: { id: listItem.product.id } })"
+    @touchend="detectDoubleTapClosure($event)"
+  >
+    <div class="right-pane">
+      <div>{{ listItem.product.title }}</div>
+      <div>
+        <div>{{ amount }} / {{ listItem.limit }}</div>
+        <div>
+          <p v-for="place in places" :key="place.id">
+            {{ place.title }}
+          </p>
+        </div>
+      </div>
+      <div>
+        <span>Общая стоимость</span>
+        <span>11 500,00₽</span>
+      </div>
+    </div>
+    <div class="left-pane">
+      <button><MoreIcon /></button>
+      <button><FavoriteIcon /></button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+.inventory-item
+  padding: 16px
+  border-radius: 8px
+  background-color: #F3F3F4
+  display: flex
+  gap: 8px
+
+.right-pane
+  flex-grow: 1
+  display: flex
+  flex-direction: column
+  gap: 16px
+  font-size: 14px
+  font-weight: 600
+  > div:nth-child(1)
+    color: #121212
+    font-size: 14px
+    font-weight: 600
+  > div:nth-child(2)
+    display: flex
+    justify-content: space-between
+    > div:nth-child(2)
+      display: flex
+      flex-direction: column
+  > div:nth-child(3)
+      display: flex
+      justify-content: space-between
+.left-pane
+  flex-grow: 0
+  flex-basis: 24px
+  display: flex
+  flex-direction: column
+  justify-content: space-between
+  align-items: center
+</style>
--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-import ListComponent from '../components/ListComponent.vue'
-import ListItem from '../components/ListItem.vue'
 import router from '../router'
 import { useListStore } from '../stores/list_store'
+import SwipeItem from '../components/SwipeItem.vue'
+import SwipeContainer from '../components/SwipeContainer.vue'
+import { ref } from 'vue'
+import { ListItem } from '@/types'
+import PlaceFilter from '../components/PlaceFilter.vue'
+import SearchComponent from '../components/SearchComponent.vue'
+import SwipeHintsComponent from '../components/SwipeHintsComponent.vue'
+import EmptyListHint from '../components/EmptyListHint.vue'
+import InventoryItem from '../components/InventoryItem.vue'
 
 const list_store = useListStore()
 
@@ -10,21 +17,60 @@ function singularWriteOff(product_id: number) {
   list_store.toBasketById(product_id)
   router.push('basket')
 }
+
+defineProps({
+  items: { type: Array<ListItem>, required: true },
+  itemComponent: null,
+})
+
+defineEmits(['left', 'right'])
+
+let serachQuery = ref('')
+let selectedCategories = ref([])
+
+function filter(item: ListItem) {
+  if (serachQuery.value) {
+    return item.product.title.toLowerCase().includes(serachQuery.value.toLowerCase())
+  }
+  if (selectedCategories.value.length) {
+    if (
+      !item.places.filter(
+        (value1) => selectedCategories.value.filter((value2) => value1.id == value2.id).length
+      ).length
+    ) {
+      return false
+    }
+  }
+  return true
+}
 </script>
 
 <template>
-  <ListComponent
-    :items="list_store.persistent"
-    :item-component="ListItem"
-    @left="
-      (product_id) => {
-        list_store.toBasketById(product_id)
-      }
-    "
-    @right="
-      (product_id) => {
-        singularWriteOff(product_id)
-      }
-    "
-  />
+  <div class="container control-container is-fluid">
+    <PlaceFilter v-model="selectedCategories" />
+    <div class="content">
+      <h3>Наименования</h3>
+    </div>
+    <SearchComponent v-model="serachQuery" />
+    <SwipeHintsComponent />
+    <EmptyListHint v-if="!list_store.persistent.length" />
+    <SwipeContainer>
+      <SwipeItem
+        v-for="item in list_store.persistent"
+        :key="item.product.id"
+        @right="singularWriteOff"
+        @left="list_store.toBasketById"
+      >
+        <template #right>
+          <slot name="left-icon">
+          </slot>
+        </template>
+        <template #left>
+          <slot name="right-icon">
+          </slot>
+        </template>
+        <InventoryItem :listItem="item" v-if="filter(item)" @doubleClick="$router.push({ name: 'product', params: { id: item.product.id } })"/>
+      </SwipeItem>
+    </SwipeContainer>
+  </div>
 </template>
